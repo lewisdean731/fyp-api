274c14e07c9ac053d02a757687663d999a53dc51
Lewis Dean <lewisdean731@gmail.com>
2021-05-06 22:33:40 +0100

    feat: add route to verify provided token
    [Ticket: FYP-15]

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Admin SDK
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./resources/admin-sdk-service-account.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+///////////////
+
+app.get("/api/auth/verifyToken", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.query.token)
+    .then((decodedToken) => {
+      res.send(decodedToken.uid);
+    })
+    .catch((error) => {
+      res.status(401);
+      res.send(error);
+    });
 });
