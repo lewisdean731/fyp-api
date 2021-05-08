eae45df9f3da6eea2e99453e0e160e684bc8c05b
Lewis Dean <lewisdean731@gmail.com>
2021-05-08 20:45:40 +0100

    feat: move auth route to own file
    [Ticket: FYP-24]

--- src/index.js
+++ src/index.js
@@ -2,34 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-// Admin SDK
-const admin = require("firebase-admin");
-
-const serviceAccount = require("./resources/admin-sdk-service-account.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+require('./routes/auth/auth.js')(app);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-///////////////
-
-app.get("/api/auth/verifyToken", (req, res) => {
-  console.log(`Verifying token ${req.query.token.slice(0,10)}...`)
-  admin
-    .auth()
-    .verifyIdToken(req.query.token)
-    .then((decodedToken) => {
-      res.send(decodedToken.uid);
-    })
-    .catch((error) => {
-      res.status(401);
-      res.send(error);
-    });
+  console.log(`Listening on port ${port}`);
 });
---
+++ src/routes/auth/auth.js
@@ -0,0 +1,26 @@
+// Admin SDK
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../../resources/admin-sdk-service-account.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = function(app){
+
+  app.get("/api/auth/verifyToken", (req, res) => {
+    console.log(`Verifying token ${req.query.token.slice(0,10)}...`)
+    admin
+      .auth()
+      .verifyIdToken(req.query.token)
+      .then((decodedToken) => {
+        res.send(decodedToken.uid);
+      })
+      .catch((error) => {
+        res.status(401);
+        res.send(error);
+      });
+  });
+
+  //other routes..
+}
