af5f17265a928ee3d921751d142bf8d5361c0ada
Lewis Dean <lewisdean731@gmail.com>
2021-05-08 22:26:11 +0100

    fix: export verifyToken as function
    [Ticket: FYP-24]

--- src/utils/authUtil.js
+++ src/utils/authUtil.js
@@ -1,7 +1,7 @@
 // Admin SDK
 
-module.exports = function verifyToken(token) {
-  console.log(`Verifying token ${req.query.token.slice(0, 10)}...`);
+exports.verifyToken = (token) => {
+  console.log(`Verifying token ${token.slice(0, 10)}...`);
   admin
     .auth()
     .verifyIdToken(req.query.token)
