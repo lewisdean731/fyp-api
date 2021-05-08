3fcb085fb5128529ee119db3f31df90f462c07d1
Lewis Dean <lewisdean731@gmail.com>
2021-05-08 22:05:41 +0100

    feat: implement endpoints to CRUD firestore users
    auth still needs to be added in
    
    [Ticket: FYP-24]

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,39 +1,61 @@
-import verifyToken from "../../utils/authUtil";
+require("../../utils/authUtil");
 
-// Admin SDK
-const admin = require("firebase-admin");
-
-const serviceAccount = require("../../resources/admin-sdk-service-account.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-
-module.exports = function(app){
-
-  app.route("/api/user")
-    .post(function (req, res) {
-      const docRef = db.collection('users').doc(req.body.uid)
-
-      await docRef.set({
-        'admin': req.body.admin,
-        'teams': req.body.teams
-      }).then((response) => {
-        res.send(response)
-      }).catch((error) => {
-        res.status(500)
-        res.send(error)
-      })
-    })
-    .get(function (req, res) {
-      res.send("Read user")
-    })
-    .put(function (req, res) {
-      res.send("Update user")
-    })
-    .delete(function (req, res) {
-      res.send("Delete user")
+module.exports = function (app, db) {
+  app
+    .route("/api/user/:uid")
+    .post(async function (req, res) {
+      const docRef = db.collection("users").doc(req.body.uid);
+      await docRef
+        .set({
+          admin: req.body.admin,
+          teams: req.body.teams,
+        })
+        .then((response) => {
+          res.send(response);
+        })
+        .catch((error) => {
+          res.status(500);
+          res.send(error);
+        });
     })
 
-}
+    .get(async function (req, res) {
+      const docRef = db.collection("users").doc(req.params.uid);
+      const doc = await docRef.get();
+      if (!doc.exists) {
+        res.status(404);
+        res.send("No such document!");
+      } else {
+        res.send(doc.data());
+      }
+    })
+
+    .put(async function (req, res) {
+      const docRef = db.collection("users").doc(req.params.uid);
+      await docRef
+        .set({
+          admin: req.body.admin,
+          teams: req.body.teams,
+        })
+        .then((response) => {
+          res.send(response);
+        })
+        .catch((error) => {
+          res.status(500);
+          res.send(error);
+        });
+    })
+
+    .delete(async function (req, res) {
+      const docRef = db.collection("users").doc(req.params.uid);
+      await docRef
+        .delete()
+        .then((response) => {
+          res.send(response);
+        })
+        .catch((error) => {
+          res.status(500);
+          res.send(error);
+        });
+    });
+};
