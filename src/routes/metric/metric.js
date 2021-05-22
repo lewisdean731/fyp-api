e668b520121bde5e6b32f9627301ffb66e7cbed6
Lewis Dean <lewisdean731@gmail.com>
2021-05-22 17:20:09 +0100

    feat: implement endpoint to fetch metrics
    and return as a time-series object
    
    [Ticket: FYP-41]

--- src/routes/metric/metric.js
+++ src/routes/metric/metric.js
@@ -1,9 +1,19 @@
 module.exports = function (app, db, admin) {
   app
-    .route("/api/metric/:metricid")
+    .route("/api/metric/:metricId")
     .post(async function (req, res) {})
 
-    .get(async function (req, res) {})
+    .get(async function (req, res) {
+      let timeSeries = []
+      db.collection("metrics").doc(req.params.metricId)
+      .collection(req.query.metricName).get()
+      .then((response) => {
+        response.forEach((doc) => {
+          timeSeries.push({timestamp: doc.id, ...doc.data()})
+        })
+        return res.status(200).json(timeSeries)
+      })
+    })
 
     .put(async function (req, res) {})
 
