6fb1a2770d9097aca36b1b8ecfefef101687bef6
Lewis Dean <lewisdean731@gmail.com>
2021-05-23 21:31:16 +0100

    chore: run prettier
    [Ticket: FYP-41]

--- src/routes/metric/metric.js
+++ src/routes/metric/metric.js
@@ -7,38 +7,46 @@ module.exports = function (app, db, admin) {
       if (!doc.exists) {
         return res.status(404).json({ error: "No such document" });
       }
-      await docRef.update({
-        // Set to the value given or the existing value by default
-        totalProjects: req.body.totalProjects || doc.data().totalProjects,
-        greenProjects: req.body.greenProjects || doc.data().greenProjects,
-        yellowProjects: req.body.yellowProjects || doc.data().yellowProjects,
-        redProjects: req.body.redProjects || doc.data().redProjects,
-      })
-      .then((response) => {
-        return res.json(response);
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
+      await docRef
+        .update({
+          // Set to the value given or the existing value by default
+          totalProjects: req.body.totalProjects || doc.data().totalProjects,
+          greenProjects: req.body.greenProjects || doc.data().greenProjects,
+          yellowProjects: req.body.yellowProjects || doc.data().yellowProjects,
+          redProjects: req.body.redProjects || doc.data().redProjects,
+        })
+        .then((response) => {
+          return res.json(response);
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
     })
 
     .get(async function (req, res) {
-      const timeSeriesMetrics = ['totalDependencies', 'greenDependencies', 'yellowDependencies', 'redDependencies']
+      const timeSeriesMetrics = [
+        "totalDependencies",
+        "greenDependencies",
+        "yellowDependencies",
+        "redDependencies",
+      ];
       // Differentiate for timeseries metrics
-      if(timeSeriesMetrics.includes(req.query.metricName)) {
-        let timeSeries = []
-        db.collection("metrics").doc(req.params.metricId)
-        .collection(req.query.metricName).get()
-        .then((response) => {
-          response.forEach((doc) => {
-            timeSeries.push({timestamp: doc.id, ...doc.data()})
+      if (timeSeriesMetrics.includes(req.query.metricName)) {
+        let timeSeries = [];
+        db.collection("metrics")
+          .doc(req.params.metricId)
+          .collection(req.query.metricName)
+          .get()
+          .then((response) => {
+            response.forEach((doc) => {
+              timeSeries.push({ timestamp: doc.id, ...doc.data() });
+            });
+            return res.status(200).json(timeSeries);
           })
-          return res.status(200).json(timeSeries)
-        })
-        .catch((error) => {
-          console.log(error)
-          return res.status(404).json({ error: "No such collection!" });
-        })
+          .catch((error) => {
+            console.log(error);
+            return res.status(404).json({ error: "No such collection!" });
+          });
       } else {
         const docRef = db.collection("metrics").doc(req.params.metricId);
         const doc = await docRef.get();
@@ -51,17 +59,19 @@ module.exports = function (app, db, admin) {
     })
 
     .put(async function (req, res) {
-      db.collection("metrics").doc(req.params.metricId)
-      .collection(req.body.metricName).doc((req.body.timestamp).toString())
-      .set({
-        value: req.body.value,
-      })
-      .then((response) => {
-        return res.status(200).json(response);
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
+      db.collection("metrics")
+        .doc(req.params.metricId)
+        .collection(req.body.metricName)
+        .doc(req.body.timestamp.toString())
+        .set({
+          value: req.body.value,
+        })
+        .then((response) => {
+          return res.status(200).json(response);
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
     })
 
     .delete(async function (req, res) {});
--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -44,60 +44,75 @@ module.exports = function (app, db) {
         });
 
       // create metrics for user
-      let metricRef = db.collection("metrics").doc(req.body.uid)
-      .collection('totalDependencies').doc();
-      await metricRef.set({
-        timestamp: new Date().getTime(),
-        value: 0,
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
-
-      metricRef = db.collection("metrics").doc(req.body.uid)
-      .collection('greenDependencies').doc();
-      await metricRef.set({
-        timestamp: new Date().getTime(),
-        value: 0,
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
-    
-      metricRef = db.collection("metrics").doc(req.body.uid)
-      .collection('yellowDependencies').doc();
-      await metricRef.set({
-        timestamp: new Date().getTime(),
-        value: 0,
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
-        
-      metricRef = db.collection("metrics").doc(req.body.uid)
-      .collection('redDependencies').doc();
-      await metricRef.set({
-        timestamp: new Date().getTime(),
-        value: 0,
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
-
-      metricRef = db.collection("metrics").doc(req.body.uid)
+      let metricRef = db
+        .collection("metrics")
+        .doc(req.body.uid)
+        .collection("totalDependencies")
+        .doc();
       await metricRef
-      .set({
-        totalProjects: 0,
-        greenProjects: 0,
-        yellowProjects: 0,
-        redProjects: 0,
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
+        .set({
+          timestamp: new Date().getTime(),
+          value: 0,
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
+
+      metricRef = db
+        .collection("metrics")
+        .doc(req.body.uid)
+        .collection("greenDependencies")
+        .doc();
+      await metricRef
+        .set({
+          timestamp: new Date().getTime(),
+          value: 0,
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
+
+      metricRef = db
+        .collection("metrics")
+        .doc(req.body.uid)
+        .collection("yellowDependencies")
+        .doc();
+      await metricRef
+        .set({
+          timestamp: new Date().getTime(),
+          value: 0,
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
+
+      metricRef = db
+        .collection("metrics")
+        .doc(req.body.uid)
+        .collection("redDependencies")
+        .doc();
+      await metricRef
+        .set({
+          timestamp: new Date().getTime(),
+          value: 0,
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
+
+      metricRef = db.collection("metrics").doc(req.body.uid);
+      await metricRef
+        .set({
+          totalProjects: 0,
+          greenProjects: 0,
+          yellowProjects: 0,
+          redProjects: 0,
+        })
+        .catch((error) => {
+          return res.status(500).json(error);
+        });
 
       return res.json(response);
-
     })
 
     .delete(async function (req, res) {
@@ -117,21 +132,21 @@ module.exports = function (app, db) {
         });
     });
 
-    app.route("/api/getAllUserIds").get(async function (req, res) {
-      console.log("Get All User IDs");
-      const collectionRef = db.collection("users");
-      await collectionRef
-        .get()
-        .then((snapshot) => {
-          let data = { userIds: [] };
-          snapshot.forEach((doc) => {
-            data.userIds.push(doc.id);
-          });
-          return res.json(data);
-        })
-        .catch((error) => {
-          console.log(error);
-          return res.status(500).json(error);
+  app.route("/api/getAllUserIds").get(async function (req, res) {
+    console.log("Get All User IDs");
+    const collectionRef = db.collection("users");
+    await collectionRef
+      .get()
+      .then((snapshot) => {
+        let data = { userIds: [] };
+        snapshot.forEach((doc) => {
+          data.userIds.push(doc.id);
         });
-    });
+        return res.json(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.status(500).json(error);
+      });
+  });
 };
