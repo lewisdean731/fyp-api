1ad5279da4eebbc23f87853b66cc0795d7e16e02
Lewis Dean <lewisdean731@gmail.com>
2021-05-23 17:54:56 +0100

    feat: implement update endpoint for metrics
    doesn't work for timeseries metrics as they work by creating new series
    
    [Ticket: FYP-41]

--- src/routes/metric/metric.js
+++ src/routes/metric/metric.js
@@ -1,22 +1,41 @@
 module.exports = function (app, db, admin) {
   app
     .route("/api/metric/:metricId")
-    .post(async function (req, res) {})
+    .post(async function (req, res) {
+      const docRef = db.collection("metrics").doc(req.params.metricId);
+      const doc = await docRef.get();
+      if (!doc.exists) {
+        return res.status(404).json({ error: "No such document" });
+      }
+      await docRef.update({
+        // Set to the value given or the existing value by default
+        totalProjects: req.body.totalProjects || doc.data().totalProjects,
+        greenProjects: req.body.greenProjects || doc.data().greenProjects,
+        yellowProjects: req.body.yellowProjects || doc.data().yellowProjects,
+        redProjects: req.body.redProjects || doc.data().redProjects,
+      })
+      .then((response) => {
+        return res.json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+    })
 
     .get(async function (req, res) {
       const timeSeriesMetrics = ['totalDependencies', 'greenDependencies', 'yellowDependencies', 'redDependencies']
       // Differentiate for timeseries metrics
       console.log(req.query.metricName)
       if(timeSeriesMetrics.includes(req.query.metricName)) {
-      let timeSeries = []
-      db.collection("metrics").doc(req.params.metricId)
-      .collection(req.query.metricName).get()
-      .then((response) => {
-        response.forEach((doc) => {
-          timeSeries.push({timestamp: doc.id, ...doc.data()})
+        let timeSeries = []
+        db.collection("metrics").doc(req.params.metricId)
+        .collection(req.query.metricName).get()
+        .then((response) => {
+          response.forEach((doc) => {
+            timeSeries.push({timestamp: doc.id, ...doc.data()})
+          })
+          return res.status(200).json(timeSeries)
         })
-        return res.status(200).json(timeSeries)
-      })
         .catch((error) => {
           console.log(error)
           return res.status(404).json({ error: "No such collection!" });
